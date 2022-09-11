ddf90ac872a01d4cf3fa29c9b4d51f45e8d0ec16
cristian5600 <cristycristian2010@gmail.com>
2022-09-11 17:18:33 +0300

    added button labeling

--- src/taskFactory.js
+++ src/taskFactory.js
@@ -1,5 +1,14 @@
 export const task = ()=>{
     const variable = 32;
+    let name = ``;
+    let priority = 0;
+    let description = ``;
+    let dueDate;
+    let date;
+    let parentProject;
+
+
+
     //constructor
     //      At a minimum they should have a title, 
     // description, dueDate and priority.
